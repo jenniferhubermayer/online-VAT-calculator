063425fba1beb1aaa02fd9f2d1b7eec2e83848e7
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-06-22 17:19:53 +0200

    Readme finished, JS Update

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# online-VAT-calculator
+# online-VAT-calculator | German online VAT calculator
+
 Online VAT calculator is fast and easy net amount calculator. To calculate VAT just type the amount, choose VAT 7% or 19% and press the button.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The Project](#the-project)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The Project
+
+In this project we implement a VAT calculator app using JavaScript. In Germany there are two VAT rates: 19% and 7%. With this app we can calculate net to gross or gross to net.
+
+### Screenshot
+
+![](./screenshot/Screenshot%202022-06-22%20at%2016-32-36%20Mehrwertsteuer-Rechner.png)
+
+### Links
+
+- Solution URL: [https://github.com/jenniferhubermayer/online-VAT-calculator](https://github.com/jenniferhubermayer/online-VAT-calculator)
+- Live Site URL: [https://jenniferhubermayer.github.io/online-VAT-calculator/](https://jenniferhubermayer.github.io/online-VAT-calculator/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+
+## Author
+
+- Website - [https://github.com/jenniferhubermayer](https://github.com/jenniferhubermayer)
+
+## Acknowledgments
+
+This project has been realized as part of the Super(Code) "Front-End Boot-Camp 2022".
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,17 @@ berechneMehrwertsteuer = (event) =>{
     let wasBerechnen = +document.querySelector("input[name='wasBerechnen']:checked").value;
     let mwst = +document.querySelector("input[name='mwst']:checked").value;
     let betrag = +document.querySelector("input[name='betrag']").value;
-    let mwstBetrag = document.querySelector(".mwstBetrag")
-    let bruttoBetrag = document.querySelector(".bruttoBetrag")
+    let mwstBetrag = document.querySelector(".mwstBetrag");
+    let bruttoBetrag = document.querySelector(".bruttoBetrag");
 
     switch(wasBerechnen){
         case 1:
-            mwstBetrag.innerText = `${(mwst * betrag / 100).toFixed(2)} €`;
-            bruttoBetrag.innerText = `${(mwst * betrag / 100 + betrag).toFixed(2)} €`;
+            mwstBetrag.innerText = (mwst * betrag / 100).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
+            bruttoBetrag.innerText = (mwst * betrag / 100 + betrag).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
             break;
         case 2:
-            mwstBetrag.innerText = `${(mwst * betrag / (mwst + 100)).toFixed(2)} €`;
-            bruttoBetrag.innerText = `${betrag - (mwst * betrag / (mwst + 100)).toFixed(2)} €`;
+            mwstBetrag.innerText = (mwst * betrag / (mwst + 100)).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
+            bruttoBetrag.innerText = (betrag - (mwst * betrag / (mwst + 100))).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
             break;
     }
 }
